9b29d83f145cac612bae0e697475a3cb9268bcad
Kevin Gresmer <kgresmer@gmail.com>
2018-04-19 11:25:30 -0500

    set toolbar to custom toolbar

--- app/src/main/java/com/gresmer/farklescoreboard/Scoreboard/Scoreboard.kt
+++ app/src/main/java/com/gresmer/farklescoreboard/Scoreboard/Scoreboard.kt
@@ -9,5 +9,6 @@ class Scoreboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scoreboard)
+        setSupportActionBar(findViewById(R.id.scoreboard_toolbar))
     }
 }
